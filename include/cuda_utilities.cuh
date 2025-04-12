4f5068ca406da01e29fc04c21e2cd560996370cf
Tommaso Gualtierotti <gualtierotti.tommaso@gmail.com>
2025-04-12 23:26:06 +0200

    add device info printing help function

--- include/cuda_utilities.cuh
+++ include/cuda_utilities.cuh
@@ -1,22 +1,89 @@
+#ifndef CUDA_UTILITIES_CUH
+    #define CUDA_UTILITIES_CUH
+#endif
+
 #ifndef UTILITIES_H
     #include "utilities.h"
 #endif
 
-#ifndef CUDA_UTILITIES_H
-    #define CUDA_UTILITIES_H
-#endif
-
+/**
+ * @brief Left-rotate a 32-bit integer.
+ *
+ * This macro performs a circular left rotation of a 32-bit integer by a given number of bits.
+ *
+ * @param a The 32-bit integer to rotate.
+ * @param b The number of bits to rotate.
+ */
 #define ROTLEFT(a,b) (((a) << (b)) | ((a) >> (32-(b))))
 
-#define CHECK_CUDA_ERROR(call) \
-    do { \
-        cudaError_t err = call; \
-        if (err != cudaSuccess) { \
-            fprintf(stderr, "CUDA error at %s:%d - %s\n", __FILE__, __LINE__, cudaGetErrorString(err)); \
-            exit(EXIT_FAILURE); \
-        } \
+/**
+ * @brief Macro to check for CUDA errors.
+ *
+ * This macro wraps a CUDA API call and checks for errors. If an error occurs, it prints the
+ * error message along with the file and line number, then exits the program.
+ *
+ * @param call The CUDA API function call to check.
+ */
+// #define CHECK_CUDA_ERROR(call)                                                \
+//     do {                                                                      \
+//         cudaError_t err = (call);                                             \
+//         if (err != cudaSuccess) {                                             \
+//             fprintf(stderr, "CUDA error at %s:%d - %s\n",                     \
+//                     __FILE__, __LINE__, cudaGetErrorString(err));            \
+//             exit(EXIT_FAILURE);                                               \
+//         }                                                                     \
+//         cudaError_t kernel_err = cudaGetLastError();                          \
+//         if (kernel_err != cudaSuccess) {                                      \
+//             fprintf(stderr, "Kernel launch failed at %s:%d - %s\n",          \
+//                     __FILE__, __LINE__, cudaGetErrorString(kernel_err));     \
+//             exit(EXIT_FAILURE);                                               \
+//         }                                                                     \
+//     } while (0)
+#define CHECK_CUDA_ERROR(call)                                                      \
+    do {                                                                            \
+        cudaError_t err_ = (call);                                                  \
+        if (err_ != cudaSuccess) {                                                  \
+            fprintf(stderr, "CUDA error in call '%s' at %s:%d - %s\n",             \
+                    #call, __FILE__, __LINE__, cudaGetErrorString(err_));          \
+            exit(EXIT_FAILURE);                                                     \
+        }                                                                           \
+        cudaError_t launch_err_ = cudaGetLastError();                               \
+        if (launch_err_ != cudaSuccess) {                                           \
+            fprintf(stderr, "Kernel launch error after call '%s' at %s:%d - %s\n", \
+                    #call, __FILE__, __LINE__, cudaGetErrorString(launch_err_));   \
+            exit(EXIT_FAILURE);                                                     \
+        }                                                                           \
     } while (0)
 
-void get_gpu_memory_allocation_info(size_t num_lines, size_t hash_size, size_t lengths_size, size_t *free_mem, size_t *total_avlbl_mem, size_t shift_modifier);
+/**
+ * @brief Retrieves GPU memory allocation information based on hashing workload.
+ *
+ * This function calculates the amount of free and total available GPU memory considering
+ * the required allocations for a SHA-1 hashing workload.
+ *
+ * @param num_lines Number of input lines to process.
+ * @param hash_size Size of the output hash storage.
+ * @param lengths_size Size of the input lengths array.
+ * @param free_mem Pointer to store the amount of free memory (in bytes).
+ * @param total_avlbl_mem Pointer to store the total available GPU memory (in bytes).
+ * @param shift_modifier Modifier to adjust measurement unit memory size displaying.
+ */
+void get_gpu_memory_allocation_info(size_t num_lines, size_t hash_size, size_t lengths_size, 
+                                    size_t *free_mem, size_t *total_avlbl_mem, size_t shift_modifier);
 
+/**
+ * @brief Retrieves the current free and total available GPU memory.
+ *
+ * This function queries the GPU for its available and total memory, applying a shift modifier
+ * if necessary.
+ *
+ * @param free_mem Pointer to store the amount of free memory (in bytes).
+ * @param total_avlbl_mem Pointer to store the total available GPU memory (in bytes).
+ * @param shift_modifier Modifier to adjust measurement unit memory size displaying.
+ */
 void get_gpu_memory_info(size_t *free_mem, size_t *total_avlbl_mem, size_t shift_modifier);
+
+void print_execution_time(float gpu_time, size_t host_time);
+// void print_execution_time(float nostream_gpu, float stream_gpu, size_t nostream_host, size_t stream_host, size_t divisor, size_t precision);
+
+void print_device_info();
--- src/cuda_utilities.cu
+++ src/cuda_utilities.cu
@@ -75,3 +75,50 @@ void get_gpu_memory_info(size_t *free_mem, size_t *total_avlbl_mem, size_t shift
 
     printf("Total memory GPU: %zu %s\nFree memory GPU: %zu %s\n", total_available_memory >> shift_modifier, unit, free_memory >> shift_modifier, unit);
 }
+
+// void print_execution_time(float nostream_gpu, float stream_gpu, size_t nostream_host, size_t stream_host, size_t divisor, size_t precision)
+void print_execution_time(float gpu_time, size_t host_time)
+{
+
+    // add way to decide if we are using streams or not
+    /* add enum with microseconds, milliseconds and seconds to decide */
+    /* Add decision to print in milliseconds or seconds, or something else and maybe also the number of decimal digits */
+
+    // printf("Time without streams: %.3f ms\n", nostream_gpu);
+    // printf("Time with streams: %.3f ms\n", stream_gpu);
+    // printf("Total execution time seen from host without streams: %.3fms\n", nostream_host / MSECPSEC_DIV);
+    // printf("Total execution time seen from host with streams: %.3fms\n", stream_host / MSECPSEC_DIV);
+    printf("Time of GPU: %.3f ms\n", gpu_time);
+    printf("Total execution time seen from host: %.3fms\n", gpu_time / MSECPSEC_DIV);
+}
+
+void print_device_info() 
+{
+    cudaDeviceProp prop;
+    
+    cudaGetDeviceProperties(&prop, 0);
+
+    printf("Device %d: %s\n", 0, prop.name);
+    printf("\tCompute capability: %d.%d\n", prop.major, prop.minor);
+    printf("\tTotal global memory: %.2f GB\n", prop.totalGlobalMem / (1024.0 * 1024.0 * 1024.0));
+    printf("\tTotal shared memory per block: %.2f KB\n", prop.sharedMemPerBlock / 1024.0);
+    printf("\tTotal constant memory: %.2f KB\n", prop.totalConstMem / 1024.0);
+    printf("\tWarp size: %d\n", prop.warpSize);
+    printf("\tMaximum threads per block: %d\n", prop.maxThreadsPerBlock);
+    printf("\tMaximum block dimensions: (%d, %d, %d)\n", prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2]);
+    printf("\tMaximum grid dimensions: (%d, %d, %d)\n", prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]);
+    printf("\tMaximum threads per multiprocessor: %d\n", prop.maxThreadsPerMultiProcessor);
+    printf("\tNumber of multiprocessors: %d\n", prop.multiProcessorCount);
+    printf("\tCompute cores: %d\n", prop.multiProcessorCount * 64);
+    printf("\tClock rate: %.2f GHz\n", prop.clockRate / 1.0e6);
+    printf("\tMemory clock rate: %.2f GHz\n", prop.memoryClockRate / 1.0e6);
+    printf("\tMemory bus width: %d bits\n", prop.memoryBusWidth);
+    printf("\tL2 cache size: %.2f KB\n", prop.l2CacheSize / 1024.0);
+    printf("\tTexture alignment: %zu bytes\n", prop.textureAlignment);
+    printf("\tPCI bus ID: %d\n", prop.pciBusID);
+    printf("\tPCI device ID: %d\n", prop.pciDeviceID);
+    printf("\tPCI domain ID: %d\n", prop.pciDomainID);
+    printf("\tUnified addressing: %s\n", prop.unifiedAddressing ? "Yes" : "No");
+    printf("\tCooperative groups: %s\n", prop.cooperativeLaunch ? "Supported" : "Not supported");
+    printf("\tECC enabled: %s\n", prop.ECCEnabled ? "Yes" : "No");
+}
