6de36b51a2d966e8d30fd03a82a7228edb211488
Tommaso Gualtierotti <gualtierotti.tommaso@gmail.com>
2025-04-18 13:26:25 +0200

    config update

--- include/config.h
+++ include/config.h
@@ -4,17 +4,17 @@
 
 #define FILENAME_PATH "dataset/all_patterns.dict"
 
-#define BATCH_NUM_LINES 100000ULL   // at the moment if the batch size is bigger than the number of lines of the file, the batch reading does not work.
+#define BATCH_NUM_LINES 1000000ULL   // at the moment if the batch size is bigger than the number of lines of the file, the batch reading does not work.
                                     // if the number of lines of the file is smaller than the number of chunks the streams execution of the functions crashes
 
 #define MAX_STRING_LENGTH 30ULL
 
 #ifndef USE_STREAMS
-#define USE_STREAMS
+// #define USE_STREAMS
 #endif
 
 #define NUM_STREAMS 8
-#define CHUNKS 8
+#define CHUNKS 64
 
 #define THREADS_PER_BLOCK 512ULL
 
